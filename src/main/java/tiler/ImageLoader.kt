03da68e0cb66d2cd8cc2b1876296f8bdbc3a6594
anastasia-zaitsewa <anastasia.zaitsewa@gmail.com>
2017-05-11 16:40:32 +0200

    Tile captions

--- src/main/java/tiler/ImageLoader.kt
+++ src/main/java/tiler/ImageLoader.kt
@@ -1,17 +1,11 @@
 package tiler
 
-import java.awt.image.BufferedImage
 import java.io.File
-import javax.imageio.ImageIO
 
 class ImageLoader {
-    fun loadImage(path: String): BufferedImage {
-        return ImageIO.read(File(path))
-    }
 
-    fun getAllImagesFromFolder(folderPath: String): List<String> {
+    fun getAllImageFilesFromFolder(folderPath: String): List<File> {
         return (File(folderPath).listFiles() ?: emptyArray())
                 .toList()
-                .map { it.absolutePath }
     }
 }
