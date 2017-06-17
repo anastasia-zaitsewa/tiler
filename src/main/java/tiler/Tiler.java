1d69bd7a0b902d3ca874fc88333ccc7faf4c7600
anastasia-zaitsewa <anastasia.zaitsewa@gmail.com>
2017-06-17 11:49:53 +0200

    Removing most of the logic from Java

--- src/main/java/tiler/Tiler.java
+++ src/main/java/tiler/Tiler.java
@@ -1,44 +1,14 @@
 package tiler;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import tiler.interactor.getters.GetTilesFromFolderUC;
-import tiler.repository.JavaFileRepository;
-import tiler.ui.canvas.CanvasGrid;
-import tiler.ui.gallery.TileGalleryPresenter;
-import tiler.ui.gallery.TileGalleryViewImpl;
 
 public class Tiler extends Application {
 
-    TileGalleryViewImpl tileGalleryView = new TileGalleryViewImpl();
 
     @Override
     public void start(Stage primaryStage) {
-        BorderPane root = new BorderPane();
-
-        CanvasGrid canvasGrid = new CanvasGrid();
-        root.setCenter(canvasGrid);
-
-        ScrollPane scrollPane = new ScrollPane();
-        scrollPane.setFitToHeight(true);
-        scrollPane.prefViewportWidthProperty().bind(tileGalleryView.widthProperty());
-        scrollPane.setContent(tileGalleryView);
-
-        root.setLeft(scrollPane);
-
-        new TileGalleryPresenter(new GetTilesFromFolderUC(new JavaFileRepository()))
-                .start(tileGalleryView);
-
-        Scene scene = new Scene(root, 800.0, 500.0);
-
-        primaryStage.setTitle("TileGallery");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        new TilerKotlin().start(primaryStage);
     }
 
 
---
+++ src/main/java/tiler/TilerKotlin.kt
@@ -0,0 +1,51 @@
+package tiler
+
+import javafx.application.Application
+import javafx.scene.Scene
+import javafx.scene.control.ScrollPane
+import javafx.scene.layout.BorderPane
+import javafx.stage.Stage
+import tiler.interactor.getters.GetTilesFromFolderUC
+import tiler.repository.JavaFileRepository
+import tiler.ui.canvas.CanvasGrid
+import tiler.ui.gallery.TileGalleryPresenter
+import tiler.ui.gallery.TileGalleryViewImpl
+
+
+/**
+ * Highest class representing main application screen,
+ * written on Kotlin.
+ * [Tiler] exists to pledge needs for statics during [Application] use
+ */
+class TilerKotlin {
+
+    val tileGalleryView = TileGalleryViewImpl()
+
+    fun start(primaryStage: Stage) {
+
+        val root = initRoot()
+
+        TileGalleryPresenter(GetTilesFromFolderUC(JavaFileRepository()))
+                .start(tileGalleryView)
+
+        primaryStage.apply {
+            title = "Tiler"
+            scene = Scene(root, 800.0, 500.0)
+            show()
+        }
+    }
+
+    private fun initRoot(): BorderPane {
+        val scrollPaneGallery = ScrollPane().apply {
+            isFitToHeight = true
+            prefViewportWidthProperty().bind(tileGalleryView.widthProperty())
+            content = tileGalleryView
+        }
+
+        val root = BorderPane().apply {
+            left = scrollPaneGallery
+            center = CanvasGrid()
+        }
+        return root
+    }
+}
